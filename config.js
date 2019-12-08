26a2389d9096baa67d11d263cf012eefdfdc3e3e
Sujin Lee <sujinlee@Sujins-MacBook-Pro-2.local>
2019-12-08 06:08:05 -0500

    trying

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const config = {
     host: "cleardb.net",
     port: 8889, // 8889 for mac
     uname: "ba4df3ea08967b",
-    pword: "heroku_9187086e92aeed2", // leave blank for windows
+    pword: "9187086e92aeed2", // leave blank for windows
     database: "db_about"
 }
 
